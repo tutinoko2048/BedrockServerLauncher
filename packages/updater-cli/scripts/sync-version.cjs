284160a689ec8e3a2e6b4fa75ecb146a7bedd97c
RetoRuto9900K <tutinoko2048@gmail.com>
2025-07-23 00:47:16 +0900

    fix: sync-version script

--- packages/updater-cli/scripts/sync-version.cjs
+++ packages/updater-cli/scripts/sync-version.cjs
@@ -5,12 +5,12 @@ const path = require('path');
 
 // updater-coreのバージョンを取得
 const corePackageJson = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '../updater-core/package.json'), 'utf8')
+  fs.readFileSync(path.join(__dirname, '../../updater-core/package.json'), 'utf8')
 );
 
 // 現在のpackage.jsonを読み込み
 const currentPackageJson = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8')
+  fs.readFileSync(path.join(__dirname, '../package.json'), 'utf8')
 );
 
 // バージョンを更新
@@ -18,7 +18,7 @@ currentPackageJson.version = corePackageJson.version;
 
 // ファイルに書き戻し
 fs.writeFileSync(
-  path.join(__dirname, 'package.json'),
+  path.join(__dirname, '../package.json'),
   JSON.stringify(currentPackageJson, null, 2) + '\n'
 );
 
